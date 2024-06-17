32913b6db7b16ea0d4b9693538adae6382c9a784
tiago.mateus <tiago.mateus@cge.ce.gov.br>
2024-06-17 00:37:09 -0300

    ajustes

--- server.js
+++ server.js
@@ -6,6 +6,15 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const PORT = process.env.PORT || 3000;
 
+// Middleware to enable CORS
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'https://visio-frontend-xi.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.get('/', (req, res) => {
     res.write(`<h1>Socket IO Start on Port : ${PORT}</h1>`);
     res.end();
@@ -19,5 +28,5 @@ io.on('connection', (socket) => {
 });
 
 server.listen(PORT, () => {
-    console.log('listening on *:3000');
+    console.log(`Listening on *:${PORT}`);
 });
