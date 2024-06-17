c37b1278af1b05e0ca824dc352ed0d4d0027ff14
tiago.mateus <tiago.mateus@cge.ce.gov.br>
2024-06-17 00:52:51 -0300

    testre

--- server.js
+++ server.js
@@ -1,33 +1,26 @@
 const express = require('express');
 const http = require('http');
-const cors = require('cors');
-const { Server } = require("socket.io");
+const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
-const io = new Server(server);
-const PORT = process.env.PORT || 3000;
-
-// Allow requests from specific origins
-const corsOptions = {
-    origin: '*',
-    methods: ['GET', 'POST'],
-    allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use(cors(corsOptions));
-
-app.get('/', (req, res) => {
-    res.send(`<h1>Socket IO Start on Port : ${PORT}</h1>`);
-});
+const io = socketIo(server);
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
-    socket.on('message', (msg) => {
-        io.emit('message', msg);
-    });
+  console.log('A user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  });
+
+  socket.on('chat message', (msg) => {
+    console.log('Message: ' + msg);
+    io.emit('chat message', msg); // Broadcast the message to everyone
+  });
 });
 
+const PORT = process.env.PORT || 3000;
+
 server.listen(PORT, () => {
-    console.log(`Listening on *:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
