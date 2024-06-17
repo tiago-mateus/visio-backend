2acc39307bd44db2cc03a195efe8cfb71ec0dbf9
tiago.mateus <tiago.mateus@cge.ce.gov.br>
2024-06-17 00:58:35 -0300

    teste

--- server.js
+++ server.js
@@ -1,26 +1,43 @@
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
-
+const cors = require('cors');
 const app = express();
 const server = http.createServer(app);
-const io = socketIo(server);
+const io = socketIo(server, {
+  cors: {
+    origin: '*', // ou substitua '*' pela URL específica do frontend, por exemplo: 'https://seu-frontend.vercel.app'
+    methods: ['GET', 'POST'],
+    allowedHeaders: ['Content-Type'],
+    credentials: true
+  }
+});
+
+// Use o middleware CORS
+app.use(cors({
+  origin: '*', // ou substitua '*' pela URL específica do frontend, por exemplo: 'https://seu-frontend.vercel.app'
+  methods: ['GET', 'POST'],
+  allowedHeaders: ['Content-Type'],
+  credentials: true
+}));
+
+app.get('/', (req, res) => {
+  res.send('Servidor rodando...');
+});
 
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  console.log('Novo cliente conectado');
 
-  socket.on('disconnect', () => {
-    console.log('User disconnected');
+  socket.on('message', (msg) => {
+    console.log('Mensagem recebida:', msg);
+    io.emit('message', msg);
   });
 
-  socket.on('chat message', (msg) => {
-    console.log('Message: ' + msg);
-    io.emit('chat message', msg); // Broadcast the message to everyone
+  socket.on('disconnect', () => {
+    console.log('Cliente desconectado');
   });
 });
 
-const PORT = process.env.PORT || 3000;
-
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+server.listen(3000, () => {
+  console.log('Servidor rodando na porta 3000');
 });
